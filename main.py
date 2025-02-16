5feb4e19626715ca05511a6fc5feab419bfecc1e
Evan0102 <evansu0102@gmail.com>
2025-02-16 14:32:15 +0800

    測試API練習

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ async def root():
     return {
         "message": "Hello FastAPI!"
     }
+
+@app.get("/test/{username}")
+async def username_test(username):
+    return {
+        "message": f"Hello {username}"
+    }
+
+@app.post("/test/message")
+async def message_test(username, message):
+    return {
+        "message":
+            f"Hello {username} YOUR message is {message}"
+    }

6d4b2825b6205bdc9a2ca2f9b8529833412e8bca
Evan0102 <evansu0102@gmail.com>
2025-05-04 15:31:15 +0800

    feat:使用資料庫

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Depends,HTTPException
+from fastapi import FastAPI,Depends,HTTPException, Response
 import pandas as pd
 from sqlalchemy.orm import Session
 from src.schemas import CreateMessageSchema,MessageSchema,UpdateMessageSchema
@@ -17,51 +17,31 @@ def get_db():
         yield db
     finally:
         db.close()
-    
-
-
-def load_message():
-    try:
-        df = pd.read_json("message.json")
-        return df.to_dict(orient="records")
-    except FileNotFoundError:
-        return []
-    
-
-def save_message(message):
-    df = pd.DataFrame(message)
-    df.to_json("message.json", orient= "records")
 
 
 @app.post(
     "/message",
     response_model=MessageSchema
 )
-async def create_message(body: CreateMessageSchema):
-    messages = load_message()
-    new_id = max([message['id']for message in messages], default=0) +1
-
-    new_message = MessageSchema(
-    id = new_id,
-    username = body.username,
-    message = body.message
-    ).dict()
-    messages.append(new_message)
-
-    save_message(messages)
+async def create_message(body: CreateMessageSchema, db: Session = Depends(get_db)):
+    new_message = Message(    
+        username = body.username,
+        message = body.message
+    )
+    db.add(new_message)
+    db.commit()
+    db.refresh(new_message)
     return new_message
-
 
 @app.get(
     "/message/{message_id}",
     response_model=MessageSchema
 )
-def get_message(message_id: int):
-    messages = load_message()
-    for index in messages:
-        if index["id"] == message_id:
-            return index
-    raise HTTPException(status_code=404, detail="找不到訊息")
+def get_message(message_id: int, db: Session = Depends(get_db)):
+    messages = db.query(Message).filter(Message.id == message_id).first()
+    if not messages:
+        raise HTTPException(status_code=404, detail="找不到訊息")
+    return messages
 
 
 @app.get(
@@ -78,27 +58,27 @@ def get_all_messages(db:Session = Depends(get_db)):
 @app.delete(
     "/message/{message_id}",
 )
-def delete_message(message_id: int):
-    messages = load_message()
-    for index, message in enumerate(messages):
-        if message["id"] == message_id:
-            del messages[index]
-            save_message(messages)
-            return HTTPException(status_code=204, detail="刪除成功")
+def delete_message(message_id: int, db: Session = Depends(get_db)):
+    messages = db.query(Message).filter(Message.id == message_id).first()
+    if not messages:
+            raise HTTPException(status_code=204, detail="刪除成功")
+    db.delete(messages)
+    db.commit()
+    return Response(status_code=204)
 
 
 @app.patch(
     "/message/{message_id}",
     response_model=MessageSchema
 )
-def update_message(message_id: int, body: UpdateMessageSchema):
-    messages = load_message()
-    for index, message in enumerate(messages):
-        if message["id"] == message_id:
-            if body.username:
-                messages[index]["username"] = body.username
-            if body.message:
-                messages[index]["message"] = body.message
-            save_message(messages)
-            return messages[index]
-            raise HTTPException(status_code=404, detail="找不到訊息")
+def update_message(message_id: int, body: UpdateMessageSchema, db: Session = Depends(get_db)):
+    messages = db.query(Message).filter(Message.id == message_id).first()
+    if not messages:
+        raise HTTPException(status_code=404, detail="找不到訊息")
+    if body.username:
+        messages.username = body.username
+    if body.message:
+        messages.message = body.message
+    db.commit()
+    db.refresh(messages)
+    return messages
